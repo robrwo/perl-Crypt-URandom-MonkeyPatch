401a44b473637d3a08c9229c9580bac2772d1f89
Robert Rothenberg <rrwo@cpan.org>
2025-04-16 17:15:18 +0100

    Split rand() out into a separate function

--- lib/Crypt/URandom/MonkeyPatch.pm
+++ lib/Crypt/URandom/MonkeyPatch.pm
@@ -6,14 +6,14 @@ use warnings;
 use Crypt::URandom qw( urandom );
 
 BEGIN {
+    *CORE::GLOBAL::rand = \&rand;
+}
 
-    *CORE::GLOBAL::rand = sub {
-        my $a = shift || 1;
-        my ($b) = unpack( "N", urandom(4) );
-        say STDERR __PACKAGE__ . "::urandom used" if $ENV{CRYPT_URANDOM_MONKEYPATCH_DEBUG};
-        return $a * $b / 0xffffffff;
-    };
-
+sub rand {
+    my $a = shift || 1;
+    my ($b) = unpack( "N", urandom(4) );
+    say STDERR __PACKAGE__ . "::urandom used" if $ENV{CRYPT_URANDOM_MONKEYPATCH_DEBUG};
+    return $a * $b / 0xffffffff;
 }
 
 =head1 SEE ALSO
