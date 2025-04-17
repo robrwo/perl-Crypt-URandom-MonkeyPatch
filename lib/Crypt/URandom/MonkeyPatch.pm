225c459be7ee306357a37913b9cc73c1e849a9ad
Robert Rothenberg <rrwo@cpan.org>
2025-04-17 16:13:35 +0100

    Undo ckeck for ivsize
    It doesn't improve security and only adds complixity and potential bugs.

--- lib/Crypt/URandom/MonkeyPatch.pm
+++ lib/Crypt/URandom/MonkeyPatch.pm
@@ -3,10 +3,9 @@ package Crypt::URandom::MonkeyPatch;
 use v5.16;
 use warnings;
 
-use Config;
 use Crypt::URandom qw( urandom );
 
-use constant SIZE => 2**( $Config{ivsize} * 8 );
+use constant SIZE => 2**32;
 use constant MASK => SIZE - 1;
 
 BEGIN {
@@ -16,13 +15,7 @@ BEGIN {
 
 sub rand(;$) {
     my $a = shift || 1;
-
-    state $bytes =
-      $Config{ivsize} == 8
-      ? sub { unpack( "Q", urandom(8) ) }
-      : sub { unpack( "N", urandom(4) ) };
-
-    my ($b) = $bytes->() & MASK;
+    my ($b) = unpack( "N", urandom(4) ) & MASK;
     say STDERR __PACKAGE__ . "::urandom used" if $ENV{CRYPT_URANDOM_MONKEYPATCH_DEBUG};
     return $a * $b / SIZE;
 }
