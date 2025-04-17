00e0d045a040820375e25f0e3627a09b029f907e
Robert Rothenberg <rrwo@cpan.org>
2025-04-17 13:43:18 +0100

    Adjust random bytes size based on the compiled ivsize

--- lib/Crypt/URandom/MonkeyPatch.pm
+++ lib/Crypt/URandom/MonkeyPatch.pm
@@ -3,19 +3,28 @@ package Crypt::URandom::MonkeyPatch;
 use v5.16;
 use warnings;
 
+use Config;
 use Crypt::URandom qw( urandom );
 
+use constant SIZE => 2**( $Config{ivsize} * 8 );
+use constant MASK => SIZE - 1;
+
 BEGIN {
+
     *CORE::GLOBAL::rand = \&rand;
 }
 
-use constant MASK => 0x7fffffff;
-
 sub rand(;$) {
     my $a = shift || 1;
-    my ($b) = unpack( "N", urandom(4) ) & MASK;
+
+    state $bytes =
+      $Config{ivsize} == 8
+      ? sub { unpack( "Q", urandom(8) ) }
+      : sub { unpack( "N", urandom(4) ) };
+
+    my ($b) = $bytes->() & MASK;
     say STDERR __PACKAGE__ . "::urandom used" if $ENV{CRYPT_URANDOM_MONKEYPATCH_DEBUG};
-    return $a * $b / ( 1 + MASK ); # ensure  ratio is < 1
+    return $a * $b / SIZE;
 }
 
 =head1 SEE ALSO
