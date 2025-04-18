c624f8145060f4edeffad223d4d6ca363966a13c
Robert Rothenberg <rrwo@cpan.org>
2025-04-18 14:16:47 +0100

    Add some documentation

--- lib/Crypt/URandom/MonkeyPatch.pm
+++ lib/Crypt/URandom/MonkeyPatch.pm
@@ -20,6 +20,38 @@ sub rand(;$) {
     return $a * $b / SIZE;
 }
 
+=head1 SYNOPSIS
+
+    use Crypt::URandom::MonkeyPatch;
+
+=head1 DESCRIPTION
+
+This module globlly overrides the builtin Perl function C<rand> with one based on the operating system's cryptographic
+random number source, e.g. F</dev/urandom>.
+
+The purpose of this module is monkey patch legacy code that uses C<rand> for security purposes.
+
+You can verify that it is working by running code with the C<CRYPT_URANDOM_MONKEYPATCH_DEBUG> environment variable set,
+e.g.
+
+    local $ENV{CRYPT_URANDOM_MONKEYPATCH_DEBUG} = 1;
+
+    my $salt = random_string("........");
+
+Every time the C<rand> function is called, it will output a line
+
+    Crypt::URandom::MonkeyPatch::urandom used
+
+This module is not intended for use with new code, or for use in CPAN modules.  If you are writing new code that needs a
+secure souce of random bytes, then use L<Crypt::URandom> or see the L<CPAN Author's Guide to Random Data for
+Security|https://security.metacpan.org/docs/guides/random-data-for-security.html>.
+
+=head1 EXPORTS
+
+=head2 rand
+
+This globally overrides the builtin C<rand> function.
+
 =head1 SEE ALSO
 
 L<Crypt::URandom>
