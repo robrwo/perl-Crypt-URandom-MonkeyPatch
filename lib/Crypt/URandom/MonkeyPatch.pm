53abbe9715667b3154081fceb2a33f6f8e779591
Robert Rothenberg <rrwo@cpan.org>
2025-04-18 15:09:09 +0100

    Add a reference to the security policy to the BUGS section

--- lib/Crypt/URandom/MonkeyPatch.pm
+++ lib/Crypt/URandom/MonkeyPatch.pm
@@ -71,6 +71,13 @@ L<CORE>
 
 L<perlfunc>
 
+=head1 append:BUGS
+
+=head2 Reporting Security Vulnerabilities
+
+Security issues should not be reported on the bugtracker website. Please see F<SECURITY.md> for instructions how to
+report security vulnerabilities
+
 =cut
 
 1;
